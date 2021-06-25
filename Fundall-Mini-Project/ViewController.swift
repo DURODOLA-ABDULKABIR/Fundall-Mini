7d71dce2f5949f9c7f6ef469cb384941cdf016a1
Durodola <durodolaabdulkabir@gmail.com>
2021-06-25 23:37:32 +0100

    Initial Commit

---
+++ Fundall-Mini-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fundall-Mini-Project
+//
+//  Created by Decagon on 25.6.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
