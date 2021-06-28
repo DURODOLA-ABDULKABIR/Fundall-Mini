044683d0ff5f208b13ab8dbc251c0549ad0a2dbc
Durodola <durodolaabdulkabir@gmail.com>
2021-06-28 15:50:58 +0100

    troubleshooting error

---
+++ Fundall-Mini-Project/UserCard/UserCardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserCardCollectionViewCell.swift
+//  Fundall-Mini-Project
+//
+//  Created by Decagon on 28.6.21.
+//
+
+import UIKit
+
+class UserCardCollectionViewCell: UICollectionViewCell {
+        @IBOutlet weak var cellText: UILabel!
+}
---
+++ Fundall-Mini-Project/UserCard/UserCardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserCardViewController.swift
+//  Fundall-Mini-Project
+//
+//  Created by Decagon on 28.6.21.
+//
+
+import UIKit
+
+class UserCardViewController: UIViewController {
+
+    //@IBOutlet weak var topBarView: UIView!
+    @IBOutlet weak var cardsCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cardsCollectionView.dataSource = self
+    }
+    
+    let cards = ["Fundall Lifestyle Card", "Rave Dollar Card", "Bitcoin Wallet" ]
+    
+}
+
+
+extension UserCardViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = cardsCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.layer.cornerRadius = 10
+        cell.cellText.text = cards[indexPath.row]
+        return cell
+    }
+
+}
